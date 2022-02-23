ba3391f0f6b44c56d248d701ea2bd514c3366ac3
jayxiaohe <495302067@qq.com>
2022-02-24 00:56:57 +0800

    modify CI tools-5

--- deploy.sh
+++ deploy.sh
@@ -9,8 +9,10 @@ cd ./public
 
 if [ -z "$GITHUB_TOKEN" ]; then
   msg='来自github action的自动部署-1'
+  githubUrl=git@github.com:jayxiaohe/jayxiaohe.github.io.git
 else
   msg='来自github action的自动部署-2'
+  githubUrl=https://jayxiaohe:${GITHUB_TOKEN}@github.com/jayxiaohe/jayxiaohe.github.io.git
   git config --global user.name "jayxiaohe"
   git config --global user.email "495302067@qq.com"
 fi
@@ -21,7 +23,8 @@ git add -A
 git commit -m "${msg}"
 # git push -f git@github.com:jayxiaohe/jayxiaohe.github.io.git master:gh-pages
 # 这种模式下只能用https提交！
-git push -f https://github.com/jayxiaohe/jayxiaohe.github.io.git master:gh-pages
+git push -f $githubUrl master:gh-pages # 推送到github
+#git push -f https://github.com/jayxiaohe/jayxiaohe.github.io.git master:gh-pages
 
 cd -
 rm -rf public/
