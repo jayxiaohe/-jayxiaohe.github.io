ac71e96a4680a02d4c3ddd49ec09b86742c4f807
jayxiaohe <495302067@qq.com>
2022-02-24 00:31:23 +0800

    modify CI tools-4

--- deploy.sh
+++ deploy.sh
@@ -4,15 +4,22 @@ set -e
 
 # 生成静态文件
 npm run build
-
 # 进入生成的文件夹
 cd ./public
 
+if [ -z "$GITHUB_TOKEN" ]; then
+  msg='来自github action的自动部署-1'
+else
+  msg='来自github action的自动部署-2'
+  git config --global user.name "jayxiaohe"
+  git config --global user.email "495302067@qq.com"
+fi
+
+
 git init
 git add -A
-git commit -m '来自deploy.sh的commit'
-
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git commit -m "${msg}"
 git push -f git@github.com:jayxiaohe/jayxiaohe.github.io.git master:gh-pages
 
 cd -
+rm -rf public/
