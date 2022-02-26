f89409cb9b8545a6e8933e31df4b8ef48a7e0559
jayxiaohe <495302067@qq.com>
2022-02-27 00:50:32 +0800

    change secret

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -126,13 +126,16 @@ module.exports = {
 	vssueConfig: {
 	  showComment: false,
 	  locale: 'zh',
-      platform: 'github',
+      /**platform: 'github',
       owner: 'jayxiaohe',
-      repo: 'blog_comments',
-      //clientId: 'd987083397f3017cd1c0',
-      //clientSecret: '5668ff799fca28547abe3d26115fd19a683d90cd'	  
+      repo: 'blog_comments',      
 	  clientId: '9663b810da543d1a1dc1',
-	  clientSecret: '22d747bf02709b75bfdcc0a31faa37f87ce67548'
+	  clientSecret: '22d747bf02709b75bfdcc0a31faa37f87ce67548'*/
+	  platform: 'gitee',
+      owner: 'hept',
+      repo: 'blog_comments',      
+	  clientId: '850b057dd4fe075874b55e343cc8365aeed9c7081da0bb030c828a6c5adcb968',
+	  clientSecret: '87669912040b3768aa065ef5c244175967111991cef24849cac61e75a7f86ecb'
     }
   },
   "markdown": {
