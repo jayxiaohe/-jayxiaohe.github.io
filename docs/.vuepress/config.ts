fed5ddd7905dd6b068dffeddbc9f28ebc0d1bec7
jayxiaohe <495302067@qq.com>
2022-03-14 22:26:08 +0800

    日常更新

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -217,7 +217,9 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
     //   'https://cdn.jsdelivr.net/gh/xugaoyi/image_store/blog/20200507175845.jpeg',
     //   'https://cdn.jsdelivr.net/gh/xugaoyi/image_store/blog/20200507175846.jpeg'
     // ], // body背景大图，默认无。 单张图片 String | 多张图片 Array, 多张图片时每隔15秒换一张。
-    bodyBgImg: ['/img/wind.jpg'], // body背景大图，默认无。 单张图片 String | 多张图片 Array, 多张图片时每隔15秒换一张。
+    bodyBgImg: [
+		//'/img/wind.jpg'
+	], // body背景大图，默认无。 单张图片 String | 多张图片 Array, 多张图片时每隔15秒换一张。
     // bodyBgImgOpacity: 0.5, // body背景图透明度，选值 0.1~ 1.0, 默认0.5
     bodyBgImgOpacity: 1.0, // body背景图透明度，选值 0.1~ 1.0, 默认0.5
     // titleBadge: false, // 文章标题前的图标是否显示，默认true
--- vdoing/components/Footer.vue
+++ vdoing/components/Footer.vue
@@ -14,7 +14,7 @@
     <!--Vdoing主题遵循MIT协议，完全开源且免费。如果您对主题的修改并不大，希望您保留主题的链接。-->
 
     <template v-if="footer">
-      Copyright © {{ footer.createYear }}-{{ new Date().getFullYear() }}
+      Copyright © {{ footer.createYear }} - {{ new Date().getFullYear() }}
       <span v-html="footer.copyrightInfo"></span>
     </template>
   </div>
@@ -27,9 +27,75 @@ export default {
       return this.$themeConfig.social
     },
     footer() {
-      return this.$themeConfig.footer
+	  console.log("进入footer()!");
+	  		var span = document.createElement("span");
+		// 这几个字母和符号宽度比较容易变化
+		span.innerHTML = "gW@i#Q!T";
+		// 设置为不可见，但可以测量宽度
+		span.style.visibility = "hidden";
+		// 字体大小为 500px，如果宽度变化比较容易区分
+		span.style.fontSize = "500px";
+		// 设置字体
+		span.style.fontFamily = "HarmonyOS Sans SC";
+		// 添加到页面
+		document.body.appendChild(span);
+		// 获取宽度
+		var width_now = span.offsetWidth;
+		// 每 0.05 秒检查一次是否加载
+		
+		var interval_check = setInterval(function() {
+			// 宽度变化，说明字体被加载
+			if(span.offsetWidth != width_now) {
+				console.log("字体切换为 HarmonyOS Sans SC!");
+				clearInterval(interval_check);
+				// 设置字体为
+				document.getElementsByTagName("html").style.fontFamily = "HarmonyOS Sans SC";
+				// 移除 span
+				document.body.removeChild(span);
+				span = null;
+			}
+		}, 50);
+	  //this.loadFont
+	  return this.$themeConfig.footer
+	}
+  },
+
+  methods: {
+	loadFont() {
+		alert("In.......");
+		console.log("111111111111111111111111111111111");
+		console.log("111111111111111111111111111111111");
+		console.log("111111111111111111111111111111111");
+		console.log("111111111111111111111111111111111");
+		var span = document.createElement("span");
+		// 这几个字母和符号宽度比较容易变化
+		span.innerHTML = "gW@i#Q!T";
+		// 设置为不可见，但可以测量宽度
+		span.style.visibility = "hidden";
+		// 字体大小为 500px，如果宽度变化比较容易区分
+		span.style.fontSize = "500px";
+		// 设置字体
+		span.style.fontFamily = "HarmonyOS Sans SC";
+		// 添加到页面
+		document.body.appendChild(span);
+		// 获取宽度
+		var width_now = span.offsetWidth;
+		// 每 0.05 秒检查一次是否加载
+		
+		var interval_check = setInterval(function() {
+			// 宽度变化，说明字体被加载
+			if(span.offsetWidth != width_now) {
+				alert("字体切换！");
+				clearInterval(interval_check);
+				// 设置字体为
+				document.getElementsByTagName("html").style.fontFamily = "HarmonyOS Sans SC";
+				// 移除 span
+				document.body.removeChild(span);
+				span = null;
+			}
+		}, 50);
     }
-  }
+  }  
 }
 </script>
 
@@ -62,5 +128,5 @@ export default {
     padding-right: ($rightMenuWidth + 1.5rem)
 .no-sidebar .footer
   width auto
-  padding-left 1.5rem
+  padding-left 1.5rem  
 </style>
