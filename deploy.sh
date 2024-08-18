f81288d6a75357046ecdc1aceaa994588580ecda
jayxiaohe <495302067@qq.com>
2024-08-18 16:52:26 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -27,7 +27,7 @@ else
 fi
 git add -A
 git commit -m "${msg}"
-git push -f $githubUrl master:gh-pages # 推送到github gh-pages分支
+git push -f $githubUrl HEAD:gh-pages # 推送到github gh-pages分支
 
 # deploy to coding pages
 # echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
